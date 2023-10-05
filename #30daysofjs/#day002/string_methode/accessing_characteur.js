ef212294dd2a499ca4b14e6e529838fe3e9a8ac2
Ladji Timera <ladjitimera52@gmail.com>
2023-10-05 11:46:50 -1200

    ajouter exo-day002

---
+++ #30daysofjs/#day002/string_methode/accessing_characteur.js
@@ -0,0 +1,13 @@
+let string = 'JavaScript'
+let firstLetter = string[0]
+console.log(firstLetter) // J
+let deuxiemeLetter = string[1] // a
+console.log(deuxiemeLetter);
+let troisiemeLetter = string[2]// v
+console.log(troisiemeLetter);
+let neiviemeLetter = string[9]
+console.log(neiviemeLetter) // t
+let lastIndex = string.length - 1
+console.log(lastIndex) // 9
+console.log(string[lastIndex]) // t
+
